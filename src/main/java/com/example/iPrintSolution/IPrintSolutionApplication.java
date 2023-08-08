9bbbd1e85b656b9554384c4161205454c8082937
viraj <viraj.ganatra@zohomail.eu>
2023-08-08 11:50:27 +0300

    Updated push - reworking log files for Linux

--- src/main/java/com/example/iPrintSolution/IPrintSolutionApplication.java
+++ src/main/java/com/example/iPrintSolution/IPrintSolutionApplication.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 @SpringBootApplication
 public class IPrintSolutionApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(IPrintSolutionApplication.class, args);
 	}
